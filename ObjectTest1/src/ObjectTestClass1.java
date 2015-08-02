f4f01140e600e4abe0799bed89f76f758fcb3c8d
satyajit kumar <satyajit.kumar@snapdeal.com>
2015-08-02 21:17:17 +0530

    objectest file

---
+++ ObjectTest1/src/ObjectTestClass1.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author satyajit.kumar
+ */
+public class ObjectTestClass1 {
+    public int a=10;
+    public int b=11;
+    public int c=12;
+//    public static void main(String[] args) {
+//        ObjectTestClass ob=new ObjectTestClass();
+//        ObjectTestClass ob1=new ObjectTestClass();
+//        ObjectTestClass ob2=new ObjectTestClass();
+//        ob.a=100;
+//        ob.b=100;
+//        ob.c=100;
+//        System.out.println("val of a.ob=========="+ob.a);
+//        System.out.println("val of b.ob=========="+ob.b);
+//        System.out.println("val of c.ob=========="+ob.c);
+//        System.out.println("---------------------------");
+//        ob1.a=111;
+//        ob1.b=111;
+//        ob1.c=111;
+//        System.out.println("val of a.ob1=========="+ob1.a);
+//        System.out.println("val of b.ob1=========="+ob1.b);
+//        System.out.println("val of c.ob1=========="+ob1.c);
+//        System.out.println("---------------------------");
+//        System.out.println("val of a.ob2=========="+ob2.a);
+//        System.out.println("val of b.ob2=========="+ob2.b);
+//        System.out.println("val of c.ob2=========="+ob2.c);
+//        ob.methodTest();
+//        ob1.methodTest();
+//        ob2.methodTest();
+//    }
+    public void methodTest(){
+        System.out.println("val of a in method=========="+a);
+    }
+}
